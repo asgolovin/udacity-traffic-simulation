f516c1749f8dac4f4ac89086ac3aeb2cceffae44
Anastasia Golovin <asgolovin@protonmail.com>
2020-04-21 15:19:22 +0200

    TrafficLight implementation (T1-T5)

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <random>
+#include <chrono>
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
 
-/* 
+
 template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    // The received object should then be returned by the receive function.
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _cond.wait(uLock, [this] { return !_queue.empty(); });
+    // use FIFO order
+    T msg = std::move(_queue.front());
+    _queue.pop_front();
+    return msg;
 }
 
 template <typename T>
@@ -18,12 +25,14 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lock(_mutex);
+    _queue.push_back(std::move(msg));
+    _cond.notify_one();
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
 
-/* 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -34,6 +43,9 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    while (true){
+        if (_messageQueue.receive() == TrafficLightPhase::green) return;
+    }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
@@ -43,7 +55,9 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when 
+    // the public method „simulate“ is called. To do this, use the thread queue in the base class.
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -53,6 +67,31 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-}
 
-*/
+    auto t1 = std::chrono::high_resolution_clock::now();
+    auto t2(t1);
+    
+    // generate random cycle duration between 4 and 6 seconds
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(4000, 6000);
+    int cycleDuration = distribution(generator);
+
+    while (true){
+        t2 = std::chrono::high_resolution_clock::now();
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() > cycleDuration){
+            t1 = t2;
+            cycleDuration = distribution(generator);
+            switch(_currentPhase){
+                case TrafficLightPhase::green :
+                  _currentPhase = TrafficLightPhase::red;
+                  _messageQueue.send(TrafficLightPhase::red);
+                  break;
+                case TrafficLightPhase::red :
+                  _currentPhase = TrafficLightPhase::green;
+                  _messageQueue.send(TrafficLightPhase::green);
+                  break;
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+}
